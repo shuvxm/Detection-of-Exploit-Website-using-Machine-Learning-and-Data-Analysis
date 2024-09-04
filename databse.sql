23922f34907eaeb051f57c42ec0b837817b70c84
Shubham <89548582+shuvxm@users.noreply.github.com>
2024-09-04 20:45:05 +0530

    Add files via upload

---
+++ databse.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Enterprise - MySQL GUI v6.56
+MySQL - 5.5.5-10.1.13-MariaDB : Database - phishing
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`phishing` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `phishing`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `Id` int(200) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `Age` varchar(200) DEFAULT NULL,
+  `Mob` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`Id`,`Name`,`Email`,`Password`,`Age`,`Mob`) values (1,'Balaram','balaram@gmail.com','1234','26','7853011277');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
